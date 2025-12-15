8746e71bebf3ec7be97f42c410770e854f344f86
Francesco <francesco-lion@virgilio.it>
2025-12-15 19:18:28 +0100

    chore: added page Login

---
+++ Pages/Login.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace cookies_authentication.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
